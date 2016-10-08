805b8917911b69989bb4188b934c7f737d547cf6
momodae <bingo110@126.com>
2016-10-08 17:39:26 +0800

    1:some changed

--- app/src/main/java/com/cz/app/ui/DialViewFragment.java
+++ app/src/main/java/com/cz/app/ui/DialViewFragment.java
@@ -10,6 +10,7 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.cz.app.R;
+import com.cz.app.widget.GradientRelativeLayout;
 import com.cz.library.widget.DialView;
 
 import java.util.Random;
@@ -28,12 +29,17 @@ public class DialViewFragment extends Fragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
         final DialView dialView= (DialView) view.findViewById(R.id.dv_view);
         final TextView textView= (TextView) view.findViewById(R.id.tv_text);
+        final GradientRelativeLayout container= (GradientRelativeLayout) view.findViewById(R.id.container);
+        container.setAnimColors(new int[]{0xFFFF4B00,0xFFFF7D00},new int[]{0xFFFFA700,0xFFFFC400},new int[]{0xFF74C31C,0xFF9ADC11},new int[]{0xFF5298FF,0xFF7D96FE},new int[]{0xFF5298FF,0xFF7D96FE});
         dialView.setOnLevelValueChangeListener(new DialView.OnLevelValueChangeListener() {
             @Override
             public void onLevelValueChanged(int startValue, int currentValue, int endValue, int level, float fraction) {
                 Log.e(TAG,"level:"+level+" fraction:"+fraction);
+                //500 0 0.0->1.0f
+                container.setAnimFraction(level,fraction);
             }
         });
         view.findViewById(R.id.btn_start).setOnClickListener(new View.OnClickListener() {
